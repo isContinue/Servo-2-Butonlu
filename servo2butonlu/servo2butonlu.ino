52504e994f3907be55976c1da10df39b914dee3d
whereismymndd <brkcn567@gmail.com>
2017-03-21 21:28:58 +0300

    Kod açıklamaları ve Kütüphane dosyaları
    +Yenilenen algoritamaya kod açıklamaları eklendi
    +Gerekli olan kütüphane dosyları eklendi.

--- servo2butonlu/servo2butonlu.ino
+++ servo2butonlu/servo2butonlu.ino
@@ -15,141 +15,93 @@
 #define MAGENTA 0xF81F
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
-//void aciyaz(uint8_t);
+
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, A4);
 
-uint8_t sure = 15, servopin=22, sagbutonpin = 31, solbutonpin = 33;
-uint8_t simdikipos=90,oncekipos=0;
+uint8_t sure = 15, servopin=22, sagbutonpin = 31, solbutonpin = 33; // her derece arası bekleme sure değeri(ms), Servo pin numarası, sag dödürme butonpin numarası, sola söndürme buton pin murası
+uint8_t simdikipos=90,oncekipos=0; 
 Servo myservo;  // myservo nesnesini oluştur
 
 int pos = 90;    // açı başlangıç değeri 90 derece ortada başla
 
 void setup() {
-  tft.reset();
+  tft.reset(); // tft yi resetle
 
-  uint16_t identifier = tft.readID();
+  uint16_t identifier = tft.readID(); // tft idsini oku
 
-  pinMode(sagbutonpin,INPUT_PULLUP); // sag döndürme butonu giriş olarak ayarlandı
-  pinMode(solbutonpin,INPUT_PULLUP); // sola döndürme butonu giriş olarka ayarlandı
+  pinMode(sagbutonpin,INPUT_PULLUP); // sag döndürme butonu dahili pull-up giriş olarak ayarlandı
+  pinMode(solbutonpin,INPUT_PULLUP); // sola döndürme butonu dahili pull-up giriş olarka ayarlandı
   myservo.attach(servopin);  //motor servopin numaralı pine atandı
-  myservo.write(pos); // servo başlangıç değeri 90 derece uygulandı
-  tft.begin(identifier);
-  tft.fillScreen(BLACK);
-  tft.setTextColor(WHITE);
-  tft.setCursor(48,88);
-  tft.setTextSize(4);
-  tft.print("Derece");
-  tft.setCursor(66,144);
-  tft.setTextSize(6);
-  tft.print(180-pos);
+  myservo.write(pos); // servo derece başlangıç pos değeri uygulandı
+  tft.begin(identifier); // tft id den başlat
+  tft.fillScreen(BLACK); // ekranı temizle
+  tft.setTextColor(WHITE); // yazı rengi beyaz olarak seçildi
+  tft.setCursor(48,88); // Imleci stringi yazdırmak için ortalanmış kordinata al
+  tft.setTextSize(4); // puntoyu ayarla
+  tft.print("Derece"); // stringi yaz
+  tft.setCursor(66,144); //imleci 66,144 konumuna al
+  tft.setTextSize(6);  // punto değerini 6 yap
+  tft.print(180-pos); // ekrana dereceyi yazdır motor tutuş yönü ters olduğu için 180-pos
   tft.setCursor(43,168);
   tft.setTextSize(2);
   tft.print("");      
 }
 
 void loop() {
- while((digitalRead(solbutonpin) == LOW)) { // butona basıldı ise soladöndür
-    if(pos >= 180) {pos = 180; break;}
-  tft.setCursor(12,216);    
-  tft.setTextSize(2);
-  tft.fillRect(189, 144, 38, 49, BLACK); // x baslangıc, y baslangıc, genişlik, yükseklik, renk
-  tft.fillRect(12, 216, 216, 16, BLACK);
-  tft.fillTriangle(51, 192, 51, 144, 15, 168, WHITE);
-  tft.print("Motor Sola Donuyor");
-      for(pos;pos<=180;pos++){
-        myservo.write(pos);
-        tft.setCursor(66,144);
-        tft.setTextSize(6);
-        tft.fillRect(66, 144, 108, 48, BLACK); // x baslangıc, y baslangıcı, genişlik, yükseklik, renk
-        tft.print(180-pos);
-        if (digitalRead(solbutonpin) == HIGH) break;
-        delay(sure);
+ 
+ while((digitalRead(solbutonpin) == LOW)) { // butona basıldı ise sola döndür
+    if(pos >= 180) {pos = 180; break;} // derece 180den büyük ise döngüye girme.
+  tft.setCursor(12,216); // imleci 12,216 kordinatına al gerekli punto için ortalanmış değerler   
+  tft.setTextSize(2); // puntoyu 2 olarak ayarla
+  tft.fillRect(189, 144, 38, 49, BLACK); // önceki dönüş yönünü silmek için arkaplan ile aynı renkte kare oluştur (x baslangıc, y baslangıc, genişlik, yükseklik, renk)
+  tft.fillRect(12, 216, 216, 16, BLACK); // Ekranı baştan yenileyip silmek yerine gerekli yeri silmek için arka plan ile aynı renkte kare oluştur sag ok yönünü silmek için
+  tft.fillTriangle(51, 192, 51, 144, 15, 168, WHITE); // sol tarafa dönüş yönüne doğru ok(üçgen) oluştur
+  tft.print("Motor Sola Donuyor"); //imlecin olduğu yerden itibaren stringi yaz
+      for(pos;pos<=180;pos++){ // derece değeri 180den küçük veya eşit ise açıyı arttır
+        myservo.write(pos); // derece değerini servoya uygula
+        tft.setCursor(66,144); //imleci 66,144 konumuna al
+        tft.setTextSize(6); // punto değerini 6 yap
+        tft.fillRect(66, 144, 108, 48, BLACK); // Önceki derece değerini silmek için arkaplan ile aynı renkte kare oluştur (x baslangıc, y baslangıc, genişlik, yükseklik, renk)
+        tft.print(180-pos); // ekrana dereceyi yazdır motor tutuş yönü ters olduğu için 180-pos
+        if (digitalRead(solbutonpin) == HIGH) break; // buton bırakıldı ise döngüden çık
+        delay(sure); // servo hızını ayarlamak için gerkli bekleme
     }
-    if(pos >= 180) {pos = 180; break;} 
+    if(pos >= 180) {pos = 180; break;} // derece 180den büyük ise buton döngüsünden çık
   }
 
   
  while((digitalRead(sagbutonpin) == LOW)) { // butona basıldı ise sagaadöndür
-  if(pos <= 0) {pos = 0; break;}
-  tft.setCursor(12,216);    
-  tft.setTextSize(2);
-  tft.fillRect(12, 216, 216, 16, BLACK);
-  tft.fillRect(15, 144, 38, 49, BLACK);
-  tft.print("Motor Saga Donuyor");
-  tft.fillTriangle(189, 192, 189, 144, 225, 168, WHITE);
-      for(pos;pos>=0;pos--){
-        myservo.write(pos);
-        tft.setCursor(66,144);
-        tft.setTextSize(6);
-        tft.fillRect(66, 144, 108, 48, BLACK); // x baslangıc, y baslangıcı, genişlik, yükseklik, renk
-        tft.print(180-pos);
-        if (digitalRead(sagbutonpin) == HIGH) break;
-        delay(sure);
+  if(pos <= 0) {pos = 0; break;} // derece 0 dan büyük ise döngüye girme.
+  tft.setCursor(12,216); // dönüş yönünü yazdırmak için imleci 12,216 kordinatına al gerekli punto için ortalanmış değerler     
+  tft.setTextSize(2); // puntoyu 2 olarak ayarla
+  tft.fillRect(12, 216, 216, 16, BLACK); // önceki dönüş yönünü silmek için arkaplan ile aynı renkte kare oluştur (x baslangıc, y baslangıc, genişlik, yükseklik, renk)
+  tft.fillRect(15, 144, 38, 49, BLACK); // Ekranı baştan yenileyip silmek yerine gerekli yeri silmek için arka plan ile aynı renkte kare oluştur sol ok yönünü silmek için
+  tft.print("Motor Saga Donuyor"); //imlecin olduğu yerden itibaren stringi yaz
+  tft.fillTriangle(189, 192, 189, 144, 225, 168, WHITE); // sag tarafa dönüş yönüne doğru ok(üçgen) oluştur
+      for(pos;pos>=0;pos--){ // derece değeri 0 dan büyük veya eşit ise açıyı arttır
+        myservo.write(pos); // derece değerini servoya uygula
+        tft.setCursor(66,144); //imleci 66,144 konumuna al
+        tft.setTextSize(6); // punto değerini 6 yap
+        tft.fillRect(66, 144, 108, 48, BLACK); // Önceki derece değerini silmek için arkaplan ile aynı renkte kare oluştur (x baslangıc, y baslangıc, genişlik, yükseklik, renk)
+        tft.print(180-pos); // ekrana dereceyi yazdır motor tutuş yönü ters olduğu için 180-pos
+        if (digitalRead(sagbutonpin) == HIGH) break; // buton bırakıldı ise döngüden çık
+        delay(sure); // servo hızını ayarlamak için gerkli bekleme
     }
-    if(pos <= 0) {pos = 0; break;} 
-  }
- simdikipos = pos;
-if ((simdikipos != oncekipos)/*||((digitalRead(sagbutonpin) == HIGH) && (digitalRead(solbutonpin) == HIGH))*/) {
-  oncekipos = simdikipos;
-  tft.setCursor(42,216);    
-  tft.setTextSize(2);
-  tft.fillRect(189, 144, 38, 49, BLACK);
-  tft.fillRect(15, 144, 38, 49, BLACK);
-  tft.fillRect(12, 216, 216, 16, BLACK);
-  tft.print("Motor Duruyor");
-  while(1){
-    if (digitalRead(solbutonpin) == LOW) break;
-    if (digitalRead(sagbutonpin) == LOW) break;
-    }
-}
-
-}
- /* while((digitalRead(solbutonpin) == LOW)) { // butona basıldı ise soladöndür
-    pos++; // dereceyi arttır saga dönüş
-    if(pos > 180) {pos = 180; break;} // decere 180 den daha fazla olamaz dereceyi 180 de tut üstüne çıkarma
-    myservo.write(pos); // arttırılan dereceyi yaz
-  //tft.fillScreen(BLACK);
-  //tft.setCursor(48,88);
-  //tft.setTextSize(4);
-  //tft.print("Derece");
-  tft.setCursor(66,144);
-  tft.setTextSize(6);
-  tft.fillRect(66, 144, 144, 48, BLACK);
-  tft.print(180-pos);
-  tft.setCursor(12,216);
-  tft.setTextSize(2);
-  tft.fillRect(96, 216, 24, 16, BLACK);
-  tft.print("Motor Sola Donuyor"); 
-  //delay(sure); // her adım arası sure kadar geçikme yarat
+    if(pos <= 0) {pos = 0; break;} // derece 0 dan küçük ise buton döngüsünden çık 
   }
 
-  
- while((digitalRead(sagbutonpin) == LOW)) { // butona basıldı ise sagaadöndür
-    if(pos <= 0) {pos = 0; break;}
-    pos--; // dereceyi azalt sola dönüş
-    if(pos < 0) {pos = 0; break;} // decere 0 dan daha düşük olamaz dereceyi 0 da tut altına düşürme
-    myservo.write(pos); // arttırılan dereceyi yaz
-  //tft.fillScreen(BLACK);
-  //tft.setCursor(48,88);
-  //tft.setTextSize(4);
-  //tft.print("Derece");
-  tft.setCursor(66,144);
-  tft.setTextSize(6);
-  tft.fillRect(66, 144, 144, 48, BLACK); // x baslangıc, y baslangıcı, genişlik, yükseklik, renk
-  tft.print(180-pos);
-  tft.setCursor(12,216);
-  tft.setTextSize(2);
-  tft.fillRect(96, 216, 24, 16, BLACK);
-  tft.print("Motor Saga Donuyor");  
-  //delay(sure); // her adım arası sure kadar geçikme yarat
- }
-  */
-  
-/*
- * void aciyaz(uint8_t deger){
-  tft.fillScreen(BLACK);
-  tft.setCursor(50,100);
-  tft.print("ACI = ");
-  tft.print(180-deger);
+ simdikipos = pos; // şimdiki derece değerini kaydet
+if ((simdikipos != oncekipos)) { // derece değeri değiştiyse
+  oncekipos = simdikipos; // önceki değeri şimdiki değer olur
+  tft.setCursor(42,216); // dönüş yönünü yazmak imleci 42,216 konumuna al -ortalamak için belirlenmiş değerler-    
+  tft.setTextSize(2); // puntoyu 2 olarak ayarla
+  tft.fillRect(189, 144, 38, 49, BLACK); // Ekranı baştan yenileyip silmek yerine gerekli yeri silmek için arka plan ile aynı renkte kare oluştur ok yönünü silmek için
+  tft.fillRect(15, 144, 38, 49, BLACK);  // Ekranı baştan yenileyip silmek yerine gerekli yeri silmek için arka plan ile aynı renkte kare oluştur ok yönünü silmek için, Durduğu için iki yönüde siliyoruz
+  tft.fillRect(12, 216, 216, 16, BLACK); // Önceki dönüş yönünü silmek için arkaplan ile aynı renkte kare oluştur (x baslangıc, y baslangıc, genişlik, yükseklik, renk)
+  tft.print("Motor Duruyor"); // Belirlenen ortalanmış kordinata stringi yazdır
+  while(1){ // butonların ikisinede basılmamış ise beklemek için
+    if (digitalRead(solbutonpin) == LOW) break; //Butonu kontrol et sol butona basıldıysa döngüden çık
+    if (digitalRead(sagbutonpin) == LOW) break; //Butonu kontrol et sağ butona basıldıysa döngüden çık
+      }
+    }
 }
-*/
